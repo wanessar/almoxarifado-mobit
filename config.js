76c439910adba501e6d711b61325a39e433f8bf6
wanessar <wanessab.rocha@gmail.com>
2026-07-08 20:57:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Cole aqui os dois links de "Publicar na Web" (formato CSV) do Google Sheets.
 // Veja o passo a passo no README.md, seção "Atualização automática via Google Sheets".
 
-const CADASTRO_CSV_URL = "COLE_AQUI_O_LINK_CSV_DA_ABA_CADASTRO";
-const ENDERECO_CSV_URL = "COLE_AQUI_O_LINK_CSV_DA_ABA_ENDERECAMENTO";
+const CADASTRO_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSIv_V0A98zeOPbemK78cVyhxy7BEViC6gBDfaJUcibGMbLICGdF62fcmLecqH2IEls358FGA7pPU2i/pub?gid=1985419877&single=true&output=csv";
+const ENDERECO_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSIv_V0A98zeOPbemK78cVyhxy7BEViC6gBDfaJUcibGMbLICGdF62fcmLecqH2IEls358FGA7pPU2i/pub?gid=80212742&single=true&output=csv";
