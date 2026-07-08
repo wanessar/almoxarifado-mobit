e819da9558997d73501748a63ef703d04e686fc4
wanessar <wanessab.rocha@gmail.com>
2026-07-08 20:35:32 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Cole aqui os dois links de "Publicar na Web" (formato CSV) do Google Sheets.
+// Veja o passo a passo no README.md, seção "Atualização automática via Google Sheets".
+
+const CADASTRO_CSV_URL = "COLE_AQUI_O_LINK_CSV_DA_ABA_CADASTRO";
+const ENDERECO_CSV_URL = "COLE_AQUI_O_LINK_CSV_DA_ABA_ENDERECAMENTO";
